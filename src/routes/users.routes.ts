333536c34e787225678bbee71f270dabf8020556
SadhasivamVMTG <sadhasivam.vm@tolaram.com>
2026-08-17 15:47:20 +0530

    feat: implement document type management, authentication, user routing, and rate limiting with associated database schema updates

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { UsersController } from "../controllers/users.controller";
+import { isAdmin } from "../middleware/isAdmin.middleware";
 
 const router = Router();
 
+
+router.use(isAdmin);
 router.get("/", UsersController.getAll);
 router.post("/", UsersController.create);
 router.put("/:id", UsersController.update);
