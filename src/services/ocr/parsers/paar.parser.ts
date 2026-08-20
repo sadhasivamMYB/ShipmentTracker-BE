4c000c435936bcc919bc2cdb27253d01293287a8
SadhasivamVMTG <sadhasivam.vm@tolaram.com>
2026-08-20 19:08:43 +0530

    implemented/Ocr-from-Datascience

--- src/services/ocr/parsers/paar.parser.ts
+++ src/services/ocr/parsers/paar.parser.ts
@@ -2,10 +2,8 @@ import { firstMatch, normalizeText } from './utils';
 
 export function parsePaar(extractedText: any): Record<string, string | null> {
 
-    console.log(extractedText, "🔃🔃🔃🔃🏢")
-
     return {
-        refElevCode: "ELEV8-SPIRIT202600024",
+        refElevCode: extractedText?.INVOICENO,
         paarNumber: extractedText?.PAARNUMBER,
         issuedDate: extractedText?.PAARISSUEDATE,
         GoodAndServices: extractedText?.ITEMS
